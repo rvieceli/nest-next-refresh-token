e2b13012d49894ddf4839db48ad81612e2940555
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2022-01-23 19:56:39 +0100

    Add custom message to easily identify token expired failed requests

--- apps/api/src/app/auth/guards/jwt-auth.guard.ts
+++ apps/api/src/app/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info, context, status?) {
+    if (info?.name === 'TokenExpiredError') {
+      throw new UnauthorizedException('TOKEN_EXPIRED');
+    }
+
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
